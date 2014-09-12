031f022d6576af16688e02750e6e3d148104ceb3
eholtrop <evanholtrop@gmail.com>
2014-09-12 16:26:23 -0400

    Replaced MapsActivity with LocationActivity
    LocationActivity is a more concrete proof of concept
    for detecting location services on Android

---
+++ LocationTracker/src/main/java/ca/mcmaster/capstone/LocationActivity.java
@@ -0,0 +1,75 @@
+package ca.mcmaster.capstone;
+
+import android.app.Activity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class LocationActivity extends Activity {
+
+    TextView latitude;
+    TextView longitude;
+    LocationManager manager;
+    String locationProvidor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location);
+
+        manager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        LocationListener locationListener = new LocationListener() {
+            public void onLocationChanged(Location newLocation) {
+                // Called when a new location is found by the network location provider.
+
+                latitude.setText(Double.toString(newLocation.getLatitude()));
+                longitude.setText(Double.toString(newLocation.getLongitude()));
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+            public void onProviderEnabled(String provider) {}
+
+            public void onProviderDisabled(String provider) {}
+        };
+
+        manager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+        locationProvidor = LocationManager.GPS_PROVIDER;
+
+        Location location = manager.getLastKnownLocation(locationProvidor);
+
+        latitude = (TextView) findViewById(R.id.latitude);
+        longitude = (TextView) findViewById(R.id.longitude);
+
+        latitude.setText(Double.toString(location.getLatitude()));
+        longitude.setText(Double.toString(location.getLongitude()));
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        //getMenuInflater().inflate(R.menu.my, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        return super.onOptionsItemSelected(item);
+    }
+}
