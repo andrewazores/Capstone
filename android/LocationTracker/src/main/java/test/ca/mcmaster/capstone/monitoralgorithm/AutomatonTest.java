d1db57047bf7dac389899146518537bcd18305d0
Andrew Azores <andrew.azores@gmail.com>
2014-11-14 13:08:57 -0500

    Fixed config for unit testing

---
+++ android/LocationTracker/src/main/java/test/ca/mcmaster/capstone/monitoralgorithm/AutomatonTest.java
@@ -0,0 +1,65 @@
+package test.ca.mcmaster.capstone.monitoralgorithm; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* Automaton Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>Nov 14, 2014</pre> 
+* @version 1.0 
+*/ 
+public class AutomatonTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: getCurrentState() 
+* 
+*/ 
+@Test
+public void testGetCurrentState() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: advance(GlobalView gv) 
+* 
+*/ 
+@Test
+public void testAdvance() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getEvaluation() 
+* 
+*/ 
+@Test
+public void testGetEvaluation() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getEvalForState(AutomatonState state) 
+* 
+*/ 
+@Test
+public void testGetEvalForState() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
