d79985d2ba031c00817470e98543664ce0c9096f
Darren Kitamura <darren@overair.ca>
2014-11-20 16:48:26 -0500

    ES OpenGL Demo

---
+++ ESGL/app/src/main/java/darren/esgl/OpenGLRenderer.java
@@ -0,0 +1,58 @@
+package darren.esgl;
+
+import android.opengl.EGLConfig;
+import android.opengl.GLSurfaceView;
+import android.opengl.GLU;
+import android.opengl.GLSurfaceView.Renderer;
+
+import javax.microedition.khronos.opengles.GL10;
+
+
+/**
+ * Created by Kitamura on 14-11-20.
+ */
+class OpenGLRenderer implements Renderer{
+
+    private Cube mCube = new Cube();
+    private float mCubeRotation;
+
+    @Override
+    public void onSurfaceCreated(GL10 gl, javax.microedition.khronos.egl.EGLConfig eglConfig) {
+        gl.glClearColor(0.0f, 0.0f, 0.0f, 0.5f);
+
+        gl.glClearDepthf(1.0f);
+        gl.glEnable(GL10.GL_DEPTH_TEST);
+        gl.glDepthFunc(GL10.GL_LEQUAL);
+
+        gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT,
+                GL10.GL_NICEST);
+
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+        gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+        gl.glLoadIdentity();
+
+        gl.glTranslatef(0.0f, 0.0f, -10.0f);
+        gl.glRotatef(mCubeRotation, 1.0f, 1.0f, 1.0f);
+
+        mCube.draw(gl);
+
+        gl.glLoadIdentity();
+
+        mCubeRotation -= 0.15f;
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        gl.glViewport(0, 0, width, height);
+        gl.glMatrixMode(GL10.GL_PROJECTION);
+        gl.glLoadIdentity();
+        GLU.gluPerspective(gl, 45.0f, (float) width / (float) height, 0.1f, 100.0f);
+        gl.glViewport(0, 0, width, height);
+
+        gl.glMatrixMode(GL10.GL_MODELVIEW);
+        gl.glLoadIdentity();
+    }
+}
