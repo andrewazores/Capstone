8aaee815c6c76197aebf45a30b40eb7c80c43f15
Jazz Kersell <jazzkersell@gmail.com>
2014-11-21 13:28:04 -0500

    Implement Valuation class

--- android/LocationTracker/src/main/java/ca/mcmaster/capstone/monitoralgorithm/Valuation.java
+++ android/LocationTracker/src/main/java/ca/mcmaster/capstone/monitoralgorithm/Valuation.java
@@ -1,7 +1,32 @@
 package ca.mcmaster.capstone.monitoralgorithm;
 
-public class Valuation {
-    public Valuation(Valuation valuation) {
+import java.util.HashMap;
+import java.util.Map;
 
+public class Valuation {
+    public static class Value<T> {
+        private final T value;
+
+        public Value(final T value) {
+            this.value = value;
+        }
+
+        public T evaluate() {
+            return value;
+        }
+    }
+
+    private final Map<String, Value<?>> valuation = new HashMap<>();
+
+    public Valuation(String variableName, Value<?> value) {
+        this.valuation.put(variableName, value);
+    }
+
+    public Valuation(Valuation valuation) {
+        this.valuation.putAll(valuation.valuation);
+    }
+
+    public Value<?> add(String variableName, Value<?> value) {
+        return this.valuation.put(variableName, value);
     }
 }
