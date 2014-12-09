1676d25c0c76c16ff6adf65d2cfb03eed3973e2d
Andrew Azores <andrew.azores@gmail.com>
2014-12-09 02:02:24 -0500

    removed arithmetic test skeleton method, implemented elsewhere

--- android/LocationTracker/src/test/java/ca/mcmaster/capstone/monitoralgorithm/BooleanExpressionTreeTest.java
+++ android/LocationTracker/src/test/java/ca/mcmaster/capstone/monitoralgorithm/BooleanExpressionTreeTest.java
@@ -1,10 +1,5 @@
 package ca.mcmaster.capstone.monitoralgorithm;
 
-import static org.junit.Assert.assertThat;
-import static org.hamcrest.CoreMatchers.*;
-
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
@@ -13,20 +8,7 @@ import org.robolectric.annotation.Config;
 @Config(emulateSdk = 18)
 @RunWith(RobolectricTestRunner.class)
 public class BooleanExpressionTreeTest {
-    @Before
-    public void before() {
 
-    }
+    @Test public void stub() {}
 
-    @After
-    public void after() {
-
-    }
-
-    @Test
-    public void testArithmeticOperatorAdd() {
-        final double first = 1, second = 2, expected = 3,
-                result = BooleanExpressionTree.ArithmeticOperator.ADD.apply(first, second);
-        assertThat("1 + 2 should be 3", result, is(expected));
-    }
 }
