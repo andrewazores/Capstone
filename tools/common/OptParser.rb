9ad36cd23194a6e128554a24c502fd93d841fb49
Andrew Azores <andrew.azores@gmail.com>
2014-12-24 18:34:52 -0500

    extracted OptParser

---
+++ tools/common/OptParser.rb
@@ -0,0 +1,27 @@
+require 'optparse'
+
+class OptParser
+  def parse_opts
+    options = {}
+    opt_parser = OptionParser.new do |opt|
+      opt.banner = "Usage: #{$0} [OPTIONS]"
+      opt.separator ''
+      opt.separator 'OPTIONS'
+
+      opt.on('-p', '--pretty', 'pretty print output') do
+        options[:pretty] = true
+      end
+
+      opt.on('-v', '--verbose', 'verbose (debugging/verification) output') do
+        options[:verbose] = true
+      end
+
+      opt.on('-h', '--help', 'print this message') do
+        puts opt
+        exit
+      end
+    end
+    opt_parser.parse!
+    options
+  end
+end
--- tools/convert_automaton/convert_automaton.rb
+++ tools/convert_automaton/convert_automaton.rb
@@ -7,31 +7,12 @@ require 'rubygems'
 require 'bundler/setup'
 
 require 'oj'
-require 'optparse'
 
 require_relative '../common/StringWordWrap'
 require_relative '../common/AutomatonParts'
+require_relative '../common/OptParser'
 
-options = {}
-opt_parser = OptionParser.new do |opt|
-  opt.banner = "Usage: #{$0} [OPTIONS]"
-  opt.separator ''
-  opt.separator 'OPTIONS'
-
-  opt.on('-p', '--pretty', 'pretty print output') do
-    options[:pretty] = true
-  end
-
-  opt.on('-v', '--verbose', 'verbose (debugging/verification) output') do
-    options[:verbose] = true
-  end
-
-  opt.on('-h', '--help', 'print this message') do
-    puts opt
-    exit
-  end
-end
-opt_parser.parse!
+options = OptParser.new.parse_opts
 
 class FileFormatVerifier
   private
