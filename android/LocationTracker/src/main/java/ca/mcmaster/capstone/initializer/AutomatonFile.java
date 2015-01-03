59bd2b6860e7bdd206078accc4d946f819f3b5d9
Andrew Azores <andrew.azores@gmail.com>
2015-01-03 00:58:34 -0500

    Remove redundant Lombok annotations

--- android/LocationTracker/src/main/java/ca/mcmaster/capstone/initializer/AutomatonFile.java
+++ android/LocationTracker/src/main/java/ca/mcmaster/capstone/initializer/AutomatonFile.java
@@ -3,14 +3,13 @@ package ca.mcmaster.capstone.initializer;
 import java.util.HashSet;
 import java.util.Set;
 
-import lombok.Getter;
 import lombok.NonNull;
 import lombok.Value;
 
 @Value
 public class AutomatonFile {
-    @NonNull @Getter Set<String> state_names = new HashSet<>();
-    @NonNull @Getter Set<Transition> transitions = new HashSet<>();
+    Set<String> state_names = new HashSet<>();
+    Set<Transition> transitions = new HashSet<>();
 
     @Value
     public static class Transition {
