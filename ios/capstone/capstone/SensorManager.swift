72f9354e6761238d730e81c72ac418e98503ae4f
Darren Kitamura <darren@overair.ca>
2015-01-12 01:08:11 -0500

    HTTP fix

--- ios/capstone/capstone/SensorManager.swift
+++ ios/capstone/capstone/SensorManager.swift
@@ -12,20 +12,42 @@ import CoreMotion
 class SensorManager {
     
     let mySensorManager: CMMotionManager
+    let myAltimeterManager: CMAltimeter
     var accelX:Double = 0.0
     var accelY:Double = 0.0
     var accelZ:Double = 0.0
     var rotX:Double = 0.0
     var rotY:Double = 0.0
     var rotZ:Double = 0.0
+    var altitude:Float = 0.0
     
+    class var sharedInstance: SensorManager{
+        
+        struct Static {
+            static var instance: SensorManager?
+            static var token: dispatch_once_t = 0
+        }
+        
+        dispatch_once(&Static.token) {
+            Static.instance = SensorManager()
+        }
+        return Static.instance!
+    }
     
     init () {
         mySensorManager = CMMotionManager()
+        myAltimeterManager = CMAltimeter()
         
         mySensorManager.deviceMotionUpdateInterval = 0.01
         mySensorManager.accelerometerUpdateInterval = 0.01
         mySensorManager.gyroUpdateInterval = 0.01
+        
+        myAltimeterManager.startRelativeAltitudeUpdatesToQueue(NSOperationQueue.currentQueue(), withHandler: {(altimeterData: CMAltitudeData!, error:NSError!)in
+            self.outputAltimeterData(altimeterData)
+            if(error != nil) {
+                println("\(error)")
+            }
+        })
         
         mySensorManager.startAccelerometerUpdatesToQueue(NSOperationQueue.currentQueue(), withHandler: {(accelerometerData: CMAccelerometerData!, error:NSError!)in
             self.outputAccelerationData(accelerometerData.acceleration)
@@ -46,6 +68,7 @@ class SensorManager {
     }
     
     func outputAccelerationData(acceleration:CMAcceleration) {
+        println("Acceleration X: \(accelX)")
         accelX = acceleration.x
         accelY = acceleration.y
         accelZ = acceleration.z
@@ -53,9 +76,14 @@ class SensorManager {
     }
     
     func outputRotationData(rotation: CMRotationRate) {
+       // println("Rotation X: \(rotX)")
         rotX = rotation.x
         rotY = rotation.y
         rotZ = rotation.z
+    }
+    
+    func outputAltimeterData(altimiter: CMAltitudeData) {
+        altitude = altimiter.relativeAltitude.floatValue
     }
     
     
--- ios/capstone/capstone/ServicePublisher.swift
+++ ios/capstone/capstone/ServicePublisher.swift
@@ -6,9 +6,26 @@
 //  Copyright (c) 2014 DKitamura. All rights reserved.
 //
 
+import UIKit
 import Foundation
 
 class ServicePublisher: NSObject, NSNetServiceDelegate {
+    
+   
+    
+    class var sharedInstance: ServicePublisher {
+        
+        struct Static {
+            static var instance: ServicePublisher?
+            static var token: dispatch_once_t = 0
+        }
+        
+        dispatch_once(&Static.token) {
+            Static.instance = ServicePublisher(domain: "local", name: "CapstoneLocationNSD-", type: "_http._tcp")
+        }
+        return Static.instance!
+    }
+
     
     var spPort:Int32
     var spDomain:String
@@ -16,26 +33,57 @@ class ServicePublisher: NSObject, NSNetServiceDelegate {
     var spName:String
     var spService:NSNetService
     
+    let UUID: String
+    var UUIDChunk = [String]()
+    let myHTTPServer = HttpServer()
+    let sensorManager: SensorManager
+
+    
     
     init (domain: String, name: String, type: String) {
         spPort = Int32(arc4random_uniform(500) + 8000)
         spDomain = domain
         spType = type
         spName = name
-        spService = NSNetService(domain: spDomain, type: spType, name: spName, port: spPort)
-        
+        UUID = NSUUID().UUIDString
+        UUIDChunk = UUID.componentsSeparatedByString("-")
+        spService = NSNetService(domain: spDomain, type: spType, name: spName+UUIDChunk[0], port: spPort)
+        sensorManager = SensorManager.sharedInstance
+        println("ServicePublisher Initalized called: \(spName+UUIDChunk[0]) running on port: \(spPort)")
         super.init()
         
 
     }
     
+    func singletonTest() {
+        println("Test called: \(spName+UUIDChunk[0]) running on port: \(spPort)")
+        
+    }
     
+    func getServiceName() -> String {
+        return spName+UUIDChunk[0]
+    }
     
     func startService() {
          spService.delegate = self       
         spService.publish()
-        println("\(spService)")
+        println("service published")
         
-        
+    }
+    
+    
+    func startWebServer() {
+        //Start HTTP Server
+
+        self.myHTTPServer["/hello"] = {
+            
+            request in
+            var test = self.sensorManager.getAccelerationData()
+            println("\(test)")
+            return {
+                .OK(.JSON(["rotationX": self.sensorManager.rotX]))
+                }()
+        }
+        self.myHTTPServer.start(listenPort: UInt16(spPort), error:nil)
     }
 }
