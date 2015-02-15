eece79a2e0d60d2b5aac8e50eacfcea9de400068
Andrew Azores <me@andrewazor.es>
2015-02-14 19:52:18 -0500

    Add @NonNull where appropriate in JsonUtil

--- android/LocationTracker/src/main/java/ca/mcmaster/capstone/networking/util/JsonUtil.java
+++ android/LocationTracker/src/main/java/ca/mcmaster/capstone/networking/util/JsonUtil.java
@@ -9,6 +9,7 @@ import java.lang.reflect.Type;
 import ca.mcmaster.capstone.monitoralgorithm.interfaces.BinaryOperator;
 import ca.mcmaster.capstone.monitoralgorithm.interfaces.Node;
 import ca.mcmaster.capstone.monitoralgorithm.interfaces.Operator;
+import lombok.NonNull;
 
 public class JsonUtil {
 
@@ -19,11 +20,11 @@ public class JsonUtil {
             .registerTypeAdapter(BinaryOperator.class, new InterfaceAdapter<BinaryOperator>())
             .create();
 
-    public static <T> T fromJson(final String json, final TypeToken<T> type) {
+    public static <T> T fromJson(@NonNull final String json, @NonNull final TypeToken<T> type) {
         return fromJson(json, type.getType());
     }
 
-    public static <T> T fromJson(final String json, final Type type) {
+    public static <T> T fromJson(@NonNull final String json, @NonNull final Type type) {
         return GSON.fromJson(json, type);
     }
 
