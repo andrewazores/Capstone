7acf288526b66c398f1cf49b2a39de3201b8e700
Jazz Kersell <jazzkersell@gmail.com>
2015-03-08 01:15:15 -0500

    Add script to get latest log file from device.

---
+++ tools/logging/extract_newest_log.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+IFS=$'\n';
+lines=($(adb shell ls /mnt/sdcard/Logs/))
+
+for line in "${lines[@]}"; do
+    echo $line;
+    numbers=($(echo $line | cut -c8-20));
+    largest=0;
+    for i in "${numbers[@]}"; do
+        if [ "$largest" -lt "$i" ]; then
+            largest=$i;
+        fi
+    done
+done
+echo $largest;
+if [ $1 != "" ]; then
+    adb pull /mnt/sdcard/Logs/logcat_$largest.log $1;
+else
+    adb pull /mnt/sdcard/Logs/logcat_$largest.log .;
+fi
